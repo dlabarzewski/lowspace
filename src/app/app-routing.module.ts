9948bcda64fc99ae36996a7eaa7dde98552df647
Anna <anna.ojdana@gmail.com>
2023-04-16 15:51:50 +0200

    hompage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { HomepageComponentModule } from './components/homepage/homepage.component-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: HomepageComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), HomepageComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
