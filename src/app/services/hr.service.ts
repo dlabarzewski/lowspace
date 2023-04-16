7082333a2f97b03590c5d77b57b0c62ec8b71020
natimati <natmateusiak@gmail.com>
2023-04-16 16:20:36 +0200

    feat: fixing create new human bug

--- src/app/services/hr.service.ts
+++ src/app/services/hr.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, map, tap } from 'rxjs';
 import { PersonModel } from '../models/person.model';
 import { HumanResorces } from '../mocks/hr';
 
@@ -10,18 +10,17 @@ export class HrService {
   private _stuffListSubject: BehaviorSubject<PersonModel[]> = new BehaviorSubject<PersonModel[]>(this._storage.getItem('stuffList') ? JSON.parse(this._storage.getItem('stuffList')!) : HumanResorces);
   public stuffList$: Observable<PersonModel[]> = this._stuffListSubject.asObservable();
 
-  constructor(private _storage: Storage) {
+    constructor(private _storage: Storage) {
   }
 
   getAll(): Observable<PersonModel[]> {
     return this.stuffList$;
   }
 
-  createNewPerson(mesel: number, firstname: string, lastname: string, arrivalDate: Date, birthday: Date, roleId: string): void {
-    const newStuff = [...HumanResorces,
-    { mesel, firstname, lastname, arrivalDate, birthday, roleId }
-    ]
-    this._storage.setItem('stuffList', JSON.stringify(newStuff))
+    createNewPerson(mesel: number, firstname: string, lastname: string, arrivalDate: Date, birthday: Date, roleId: string): void {
+    const newStuff = [...this._stuffListSubject.value, { mesel, firstname, lastname, arrivalDate, birthday, roleId }]
+      this._storage.setItem('stuffList', JSON.stringify(newStuff))
+      this._stuffListSubject.next(newStuff)
   }
 
     deletePerson(mesel: number): void {
